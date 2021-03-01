bd2aaa932b296695a4dcb9bff7c720bb3019b680
iqrasuhail <ialmani@bsu.edu>
2021-02-28 21:19:32 -0500

    Add path to get user details

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.urls import path, include
-from .api import RegisterApi
+from .api import RegisterApi, UserApi
 urlpatterns = [
     path('api/register', RegisterApi.as_view()),
+    path('api/getUserDetails', UserApi.as_view()),
 ]
