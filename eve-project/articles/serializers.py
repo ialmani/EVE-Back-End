0b24e41d4ef388d915b34a14bf252ed75ece6473
Jamari Brown <jbrown@bsu.edu>
2021-04-12 14:29:39 -0400

    Fix articles models and zoom missing files

--- eve-project/articles/serializers.py
+++ eve-project/articles/serializers.py
@@ -6,8 +6,4 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-<<<<<<< HEAD
-        fields = ('id', 'title', 'author', ' user_id', 'content')
-=======
         fields = ('id', 'title', 'author', 'user_id', 'content')
->>>>>>> b3422c6b2aae7e0b55af53886d6ad3499308fb3d
--- eve-project/zoom/models.py
+++ eve-project/zoom/models.py
@@ -5,6 +5,6 @@ class Zoom(models.Model):
     start_time = models.TextField(max_length=300, default="")
     date = models.DateField()
     description = models.TextField(max_length=300, default="")
-    zoom_url = models.URLField(max_length=300)
-    zoom_id = models.TextField(max_length=300, default="")
+    zoom_url = models.URLField(max_length=50)
+    zoom_id = models.TextField(max_length=11, default="")
     password = models.CharField(max_length=120)
