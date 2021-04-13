883d0fd9cc690e95819b37d191199468dc261a2b
Mera Alfawares <maalfawares@bsu.edu>
2021-04-13 13:23:53 -0400

    Merge conflicts

--- eve-project/articles/serializers.py
+++ eve-project/articles/serializers.py
@@ -6,8 +6,5 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-<<<<<<< HEAD
         fields = ('id', 'title', 'author', ' user_id', 'content')
-=======
-        fields = ('id', 'title', 'author', 'user_id', 'content')
->>>>>>> 0b24e41d4ef388d915b34a14bf252ed75ece6473
+
