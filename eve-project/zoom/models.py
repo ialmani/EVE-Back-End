227af048b7df19c51331df5271fbcffc1debad3e
Mera Alfawares <maalfawares@bsu.edu>
2021-04-30 21:25:12 -0400

    Update zoom

--- eve-project/zoom/models.py
+++ eve-project/zoom/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Zoom(models.Model):
     title = models.CharField(max_length=120)
-    start_time = models.TextField(max_length=300, default="")
-    date = models.DateField()
-    description = models.TextField(max_length=300, default="")
-    zoom_url = models.URLField(max_length=50)
-    zoom_id = models.TextField(max_length=11, default="")
-    password = models.CharField(max_length=120)
+    start_date = models.DateTimeField(max_length=300)
+    end_date = models.DateTimeField(max_length=300)
+    description = models.TextField(max_length=300, default="", blank=True)
+    zoom_url = models.URLField()
+    zoom_id = models.TextField(max_length=120)
+    password = models.CharField(max_length=120, default="", blank=True)
--- eve-project/zoom/serializers.py
+++ eve-project/zoom/serializers.py
@@ -1,9 +1,12 @@
-from rest_framework import serializers
+from rest_framework import serializers, fields
 
 from .models import Zoom
 
 
 class ZoomSerializer(serializers.ModelSerializer):
+    start_date = fields.DateTimeField(input_formats=['%Y-%m-%dT%H:%M'])
+    end_date = fields.DateTimeField(input_formats=['%Y-%m-%dT%H:%M'])
+
     class Meta:
-        model = Zoom
-        fields = ('id', 'title', 'start_time', 'date','description', 'zoom_url', 'password', 'zoom_id')
+            model = Zoom
+            fields = ('id', 'title', 'start_date', 'end_date','description', 'zoom_url', 'password', 'zoom_id')
